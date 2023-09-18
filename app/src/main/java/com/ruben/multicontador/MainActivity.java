67cd923edcac36610498600e0cf1c378b961a983
ruben <rjulian@elpuig.xeill.net>
2023-09-18 15:42:31 +0200

    MultiContador_v2

--- app/src/main/java/com/ruben/multicontador/MainActivity.java
+++ app/src/main/java/com/ruben/multicontador/MainActivity.java
@@ -9,11 +9,28 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    int contador;
+    int contador1 = 0;
+    int contador2 = 0;
+    int contador3 = 0;
+    int contador4 = 0;
     TextView texto1;
-    Button boton1;
+    Button reset_all;
+
+    TextView texto2;
+    Button sum1;
     Button reset1;
+
+    TextView texto3;
+    Button sum2;
+    Button reset2;
+
+    TextView texto4;
+    Button sum3;
+    Button reset3;
+
+    TextView texto5;
+    Button sum4;
+    Button reset4;
 
 
     @Override
@@ -22,8 +39,111 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         texto1 = findViewById(R.id.text);
-        boton1 = findViewById(R.id.ResetAll);
+        reset_all = findViewById(R.id.ResetAll);
+
+        texto2 = findViewById(R.id.text2);
+        sum1 = findViewById(R.id.Boton1);
+        reset1 = findViewById(R.id.Reset1);
+
+        texto3 = findViewById(R.id.text3);
+        sum2 = findViewById(R.id.Boton2);
+        reset2 = findViewById(R.id.Reset2);
+
+        texto4 = findViewById(R.id.text4);
+        sum3 = findViewById(R.id.Boton3);
+        reset3 = findViewById(R.id.Reset3);
+
+        texto5 = findViewById(R.id.text5);
+        sum4 = findViewById(R.id.Boton4);
+        reset4 = findViewById(R.id.Reset4);
 
 
+        sum1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                contador1 ++;
+                texto1.setText(contador1 + contador2 + contador3 + contador4);
+                texto2.setText(contador1);
+            }
+        });
+
+        reset1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View vb) {
+                contador1 = 0;
+                texto1.setText(contador1 + contador2 + contador3 + contador4);
+                texto2.setText(contador1);
+            }
+        });
+
+        sum2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View vc) {
+                contador2 ++;
+                texto1.setText(contador1 + contador2 + contador3 + contador4);
+                texto3.setText(contador2);
+            }
+        });
+
+        reset2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View vd) {
+                contador2 = 0;
+                texto1.setText(contador1 + contador2 + contador3 + contador4);
+                texto3.setText(contador2);
+            }
+        });
+
+        sum3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View ve) {
+                contador3 ++;
+                texto1.setText(contador1 + contador2 + contador3 + contador4);
+                texto4.setText(contador3);
+            }
+        });
+
+        reset3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View vf) {
+                contador3 = 0;
+                texto1.setText(contador1 + contador2 + contador3 + contador4);
+                texto4.setText(contador3);
+            }
+        });
+
+        sum4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View vg) {
+                contador4 ++;
+                texto1.setText(contador1 + contador2 + contador3 + contador4);
+                texto5.setText(contador4);
+            }
+        });
+
+        reset4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View vh) {
+                contador4 = 0;
+                texto1.setText(contador1 + contador2 + contador3 + contador4);
+                texto5.setText(contador4);
+            }
+        });
+
+        reset_all.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int contador1 = 0;
+                int contador2 = 0;
+                int contador3 = 0;
+                int contador4 = 0;
+
+                texto1.setText(contador1 + contador2 + contador3 + contador4);
+                texto2.setText(contador1);
+                texto3.setText(contador2);
+                texto4.setText(contador3);
+                texto5.setText(contador4);
+            }
+        });
     }
 }
